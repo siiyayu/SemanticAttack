e6e4ef8cc1a2c050f95d776bce14aec1070ad992
Andrei Beliaev <andrewbelyaev2164@gmail.com>
2024-11-14 21:48:18 -0600

    fixed retain_graph error

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from typing import Optional
 import requests
 from io import BytesIO
 from tqdm.auto import tqdm
+from memory_profiler import profile
+
 
 
 class AttnProcessor:
@@ -17,7 +19,9 @@ class AttnProcessor:
     """
     def __init__(self):
         # Initialize an empty list to store attention maps for each call
-        self.attention_maps = []
+        # self.attention_maps = []
+        self.sum_attention_maps = None
+        self.counter = 0
         # self.hidden_states = []
         # self.encoder_hidden_states = []
         # self.attention_mask = []
@@ -73,7 +77,13 @@ class AttnProcessor:
 
         res = int(np.sqrt(attention_probs.shape[1]))
         num_tokens = attention_probs.shape[-1]
-        self.attention_maps.append(attention_probs.view(-1, res, res, num_tokens))
+        # self.attention_maps.append(attention_probs.view(-1, res, res, num_tokens))
+        attention_map = attention_probs.reshape(-1, res, res, num_tokens)
+        if self.sum_attention_maps is None:
+            self.sum_attention_maps = attention_map
+        else:
+            self.sum_attention_maps += attention_map
+        self.counter += 1
         # self.hidden_states.append(hidden_states)
         # self.encoder_hidden_states.append(encoder_hidden_states)
         # self.attention_mask.append(attention_mask)
@@ -96,33 +106,30 @@ class AttnProcessor:
 
         return hidden_states
 
-def load_image(url, size=None):
-    response = requests.get(url,timeout=1)
-    img = Image.open(BytesIO(response.content)).convert('RGB')
-    if size is not None:
-        img = img.resize(size)
-    return img
-
-
 class SemanticAttack():
     def __init__(self,
                  image,
                  editing_prompt,
                  token, device,
                  attention_processor_class=AttnProcessor,
-                 num_inference_steps_mask=50) -> None:
+                 num_inference_steps_mask=50,
+                 mask_threshold=0.5,
+                 perturbation_budget=0.06,
+                 attacking_step_size=0.01,
+                 number_of_attacking_steps=100,
+                 num_diffusion_steps=10) -> None:
 
         self.token = token
         self.editing_prompt = editing_prompt
         self.attention_processor_class = attention_processor_class
         # self.attention_maps = {}
 
-        # constants
-        self.mask_threshold = 0.5
-        self.perturbation_budget = 0.06
-        self.attacking_step_size = 0.07
-        self.number_of_attacking_steps = 10
-        self.num_diffusion_steps = 10
+        # constants (move it to the sem att function
+        self.mask_threshold = mask_threshold
+        self.perturbation_budget = perturbation_budget
+        self.attacking_step_size = attacking_step_size
+        self.number_of_attacking_steps = number_of_attacking_steps
+        self.num_diffusion_steps = num_diffusion_steps
 
         self.strength = 0.5
         self.guidance_scale = 6
@@ -166,6 +173,9 @@ class SemanticAttack():
         self.mask = None
         self.latent_shape = None
         self.text_embeddings = None
+        self.init_latent = None
+
+        self.loss = []
 
     def upsample_attention(self, attention_map, target_res):  # works
         '''
@@ -182,7 +192,7 @@ class SemanticAttack():
         attention_map = attention_map.reshape(batch_size_n_heads, n_tokens, target_res, target_res).permute(0, 2, 3, 1)
         return attention_map
 
-    def average_attention_maps(self, num_steps=1):  # works
+    def average_attention_maps(self):  # works
         '''
         Upsamples the attention maps and averages them across all layers.
         num_steps: int, the number of timesteps to average over.
@@ -190,26 +200,29 @@ class SemanticAttack():
         resolution_to_upsample_to = 64
         to_average = []
         for key, value in self.transformer_block_name_to_attention_processor_map.items():
-            attention_map = value.attention_maps[-num_steps:].copy()
-            attention_map = sum(attention_map) / len(attention_map)
-            res = attention_map.shape[1]
+            # attention_map = value.attention_maps[-num_steps:].copy()
+            # attention_map = sum(attention_map) / len(attention_map)
+            avg_attention_map = value.sum_attention_maps / value.counter
+            res = avg_attention_map.shape[1]
             if res == resolution_to_upsample_to:
-                to_average.append(attention_map)
+                to_average.append(avg_attention_map)
             else:
-                to_average.append(self.upsample_attention(attention_map, resolution_to_upsample_to))
+                to_average.append(self.upsample_attention(avg_attention_map, resolution_to_upsample_to))
         to_average = torch.cat(to_average, dim=0)
         averaged = to_average.sum(dim=0) / to_average.shape[0]
-        del to_average
-        del attention_map
+        # del to_average
+        # del attention_map
         return averaged
 
     def denoise(self):
         '''
         Generates mask using sampling from the noisy state obtained with forward pass.
         '''
+        # check_memory()
 
         # text embeddings
         text_embeddings = self.get_text_embeddings(prompt=self.token)
+        # print(get_tensor_size(text_embeddings))
 
         with torch.no_grad():
             # timesteps
@@ -222,7 +235,10 @@ class SemanticAttack():
             num_inference_steps = self.num_inference_steps_mask - t_start
 
             # latents
-            init_latent = self.vae.encode(self.preprocessed_image).latent_dist.sample(self.generator)
+            # init_latent = self.vae.encode(self.preprocessed_image).latent_dist.sample(self.generator)
+            init_latent = self.get_init_latent()
+
+            # print(get_tensor_size(init_latent))
             self.latent_shape = init_latent.shape
             init_latent = self.vae.config.scaling_factor * init_latent
             noise = torch.randn(init_latent.shape, generator=self.generator, device=self.device)
@@ -232,6 +248,7 @@ class SemanticAttack():
             for t in tqdm(timesteps, total=len(timesteps)):
                 latent_model_input = torch.cat([latent] * 2)  # cfg
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+                # print(get_tensor_size(latent_model_input))
 
                 noise_pred = self.unet(
                     latent_model_input,
@@ -251,57 +268,60 @@ class SemanticAttack():
 
     def generate_mask_using_forward_noise(self):
         self.denoise()
-        attention_map = model.average_attention_maps(num_steps=50)
+        attention_map = model.average_attention_maps()
         token_attention_map = attention_map[:, :, 1]
-        masked_token_attention_map = token_attention_map * (token_attention_map > token_attention_map.max() * 0.5)
+        masked_token_attention_map = token_attention_map * (token_attention_map > token_attention_map.max() * self.mask_threshold)
         mask = masked_token_attention_map.unsqueeze(0).unsqueeze(0).repeat(1, self.latent_shape[1], 1, 1)
         self.mask = mask
         self.clean_attention_processors()
 
     def clean_attention_processors(self):
         for key, value in self.transformer_block_name_to_attention_processor_map.items():
-            value.attention_maps = []
-
+            value.sum_attention_maps = None
+            value.counter = 0
+        # gc.collect()
 
     def get_text_embeddings(self, prompt):
         '''
         Gets text embedding with classifier-free guidance
         :return: text embeddings
         '''
-        text_input = self.tokenizer(
-            prompt,
-            padding="max_length",
-            # max_length=self.tokenizer.model_max_length,
-            max_length=10,
-            truncation=True,
-            return_tensors="pt",
-        )
 
         if self.text_embeddings is None:
 
-            # embeddings
-            text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+            text_input = self.tokenizer(
+                prompt,
+                padding="max_length",
+                # max_length=self.tokenizer.model_max_length,
+                max_length=10,
+                truncation=True,
+                return_tensors="pt",
+            )
+            with torch.no_grad():
+                # embeddings
+                text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
 
-            # classifier free guidance
-            max_length = text_input.input_ids.shape[-1]
-            uncond_input = self.tokenizer("", padding="max_length", max_length=max_length, return_tensors="pt")
-            uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+                # classifier free guidance
+                max_length = text_input.input_ids.shape[-1]
+                uncond_input = self.tokenizer("", padding="max_length", max_length=max_length, return_tensors="pt")
+                uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
 
-            text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+                text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 
-            self.text_embeddings = text_embeddings
-
-            return text_embeddings
+                self.text_embeddings = text_embeddings
 
         else:
-            return self.text_embeddings
+            text_embeddings = self.text_embeddings
 
-    def print_graph(self, fn, indent=0):
-        if fn is None:
-            return
-        print("  " * indent + str(fn))
-        for next_fn, _ in fn.next_functions:
-            self.print_graph(next_fn, indent + 1)
+        return text_embeddings
+
+    def get_init_latent(self):
+        if self.init_latent is None:
+            init_latent = self.vae.encode(self.preprocessed_image).latent_dist.sample(self.generator)
+            self.init_latent = init_latent
+        else:
+            init_latent = self.init_latent
+        return init_latent
 
     def timestep_universal_gradient_updating(self):
         '''
@@ -316,19 +336,19 @@ class SemanticAttack():
         diffusion_timesteps = self.scheduler.timesteps
 
         with torch.no_grad():
-            init_latent = self.vae.encode(self.preprocessed_image).latent_dist.sample(self.generator)
-            init_latent = self.vae.config.scaling_factor * init_latent
+            # init_latent = self.vae.encode(self.preprocessed_image).latent_dist.sample(self.generator)
+            init_latent = self.get_init_latent().to(self.device)
+            init_latent = (self.vae.config.scaling_factor * init_latent).to(self.device)
+            delta = torch.zeros(init_latent.shape).to(self.device)
 
-        delta = torch.zeros(init_latent.shape)
-
-        init_latent_adv = init_latent.clone().detach().requires_grad_(True)
 
         for attacking_step in tqdm(range(self.number_of_attacking_steps), total=self.number_of_attacking_steps,
                                    desc="Attacking steps"):
-            accumulated_grad = torch.zeros_like(init_latent_adv)  # Accumulate gradient across timesteps
+            accumulated_grad = torch.zeros(init_latent.shape).to(self.device).requires_grad_(False)  # Accumulate gradient across timesteps
             for t in tqdm(diffusion_timesteps, total=len(diffusion_timesteps),
                           desc=f"Attacking step {attacking_step + 1}"):
                 with torch.autograd.profiler.profile() as prof:
+                    init_latent_adv = init_latent.clone().detach().requires_grad_(True)
                     noise = torch.randn(init_latent_adv.shape, generator=self.generator, device=self.device)
                     noised_adv_latent = self.scheduler.add_noise(init_latent_adv, noise, t)
 
@@ -345,23 +365,27 @@ class SemanticAttack():
                         # added_cond_kwargs=added_cond_kwargs,
                         # return_dict=False,
                     )[0]
-                    del noise_pred
+                    # del noise_pred
 
                     # calculating loss
-                    attention_map = self.average_attention_maps(num_steps=1)[:, :, 1]
+                    attention_map = self.average_attention_maps()[:, :, 1]
                     if init_latent_adv.grad is not None:
                         init_latent_adv.grad.zero_()
                     loss = torch.norm(attention_map, p=1)
-                    loss.backward(retain_graph=True)
+                    self.loss.append(loss.item())
+                    # loss = torch.norm(latent_model_input)
+                    loss.backward()
 
                     ##
                     # self.print_graph(attention_map)
                     ##
                     with torch.no_grad():
                         accumulated_grad += init_latent_adv.grad
-                # print(prof.key_averages().table(sort_by="self_cpu_memory_usage"))
-            self.clean_attention_processors()
+                self.clean_attention_processors()
 
+                # print(prof.key_averages().table(sort_by="self_cpu_memory_usage"))
+                # for event in prof.function_events:
+                #     print(event)
             grad = accumulated_grad / len(diffusion_timesteps)
             delta = delta + self.attacking_step_size * torch.sign(grad)
             delta.clamp_(-self.perturbation_budget, self.perturbation_budget)
@@ -380,29 +404,42 @@ class SemanticAttack():
                 module.attn2.processor = self.attention_processor_class()
                 self.transformer_block_name_to_attention_processor_map[name] = module.attn2.processor
 
-    def show_cross_attention_maps(self, direction=["down", "up"], token=8, resolution=16):
+    def show_attention_maps(self, directions, token, resolution):
         '''
         Shows averaged cross attention across timesteps, batch * n_heads, and layers
         with the same resolution attention maps for a given resolution and direction.
-        direction: ["down", "up"]
-        token: int
-        resolution: int
+        direction: list, for example ["down", "up", "mid"]
+        token: int, for example 1
+        resolution: int for example 16
         '''
-        attention_maps = []
+        filtered_attention_sums = []
         for key, value in self.transformer_block_name_to_attention_processor.items():
             dir = key.split("_")[0]
-            res = value.attention_maps[0].shape[1]
-            if dir in direction and res == resolution:
-                for item in value.attention_maps:
-                    attention_maps.append(item)
-        attention_maps = torch.cat(attention_maps, dim=0)
-        attention_maps = attention_maps.sum(dim=0) / attention_maps.shape[0]
-        attention_maps = attention_maps[:, :, token]
-        attention_maps = 255 * attention_maps / attention_maps.max()
-        attention_maps = attention_maps.unsqueeze(-1).expand(*attention_maps.shape, 3)
-        attention_maps = attention_maps.numpy().astype(np.uint8)
-        return Image.fromarray(attention_maps).resize((256, 256))
-# Press the green button in the gutter to run the script.
+            res = value.sum_of_attention_maps.shape[1]
+            if dir in directions and res == resolution:
+                filtered_attention_sums.append(value.sum_of_attention_maps)
+        filtered_attention_sums = torch.cat(filtered_attention_sums, dim=0)
+        filtered_attention_sums = filtered_attention_sums.sum(dim=0) / filtered_attention_sums.shape[0]
+        filtered_attention_sums = filtered_attention_sums[:, :, token]
+        filtered_attention_sums = 255 * filtered_attention_sums / filtered_attention_sums.max()
+        filtered_attention_sums = filtered_attention_sums.unsqueeze(-1).expand(*filtered_attention_sums.shape, 3)
+        filtered_attention_sums = filtered_attention_sums.numpy().astype(np.uint8)
+        return Image.fromarray(filtered_attention_sums).resize((256, 256))
+
+def load_image(url, size=None):
+    response = requests.get(url,timeout=1)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    if size is not None:
+        img = img.resize(size)
+    return img
+
+# def check_memory():
+#     process = psutil.Process(os.getpid())
+#     print(f"Memory used: {process.memory_info().rss / (1024 * 1024):.2f} MB")
+
+def get_tensor_size(tensor: torch.Tensor) -> int:
+    return tensor.element_size() * tensor.nelement() / (1024 * 1024)
+
 if __name__ == '__main__':
     input_image = load_image('https://images.pexels.com/photos/8306128/pexels-photo-8306128.jpeg', size=(512, 512))
     device = "cpu"
@@ -413,6 +450,13 @@ if __name__ == '__main__':
                            attention_processor_class=AttnProcessor,
                            #  pipeline_class=StableDiffusionImg2ImgPipeline,
                            #  scheduler_class=DDIMScheduler,
-                           num_inference_steps_mask=50)
+                           num_inference_steps_mask=20,
+                           mask_threshold=0.5,
+                           perturbation_budget=0.06,
+                           attacking_step_size=0.07,
+                           number_of_attacking_steps=1,
+                           num_diffusion_steps=2
+                           )
+
     model.generate_mask_using_forward_noise()
     immunized_latent = model.timestep_universal_gradient_updating()
