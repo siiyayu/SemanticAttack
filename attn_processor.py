4ec5b895323146d743ae0f636842a70415a10b29
Andrei Beliaev <andrewbelyaev2164@gmail.com>
2024-11-15 20:22:34 -0600

    splitted files

---
+++ attn_processor.py
@@ -0,0 +1,98 @@
+from diffusers.models.attention import Attention
+from typing import Optional
+import torch
+import numpy as np
+
+
+class AttnProcessor:
+    """
+    Default processor for performing attention-related computations.
+    """
+    def __init__(self):
+        # Initialize an empty list to store attention maps for each call
+        # self.attention_maps = []
+        self.sum_attention_maps = None
+        self.counter = 0
+        # self.hidden_states = []
+        # self.encoder_hidden_states = []
+        # self.attention_mask = []
+
+    def __call__(
+        self,
+        attn: Attention,
+        hidden_states: torch.Tensor,
+        encoder_hidden_states: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        temb: Optional[torch.Tensor] = None,
+        *args,
+        **kwargs,
+    ) -> torch.Tensor:
+        if len(args) > 0 or kwargs.get("scale", None) is not None:
+            deprecation_message = "The `scale` argument is deprecated and will be ignored. Please remove it, as passing it will raise an error in the future. `scale` should directly be passed while calling the underlying pipeline component i.e., via `cross_attention_kwargs`."
+            deprecate("scale", "1.0.0", deprecation_message)
+
+        residual = hidden_states
+
+        if attn.spatial_norm is not None:
+            hidden_states = attn.spatial_norm(hidden_states, temb)
+
+        input_ndim = hidden_states.ndim
+
+        if input_ndim == 4:
+            batch_size, channel, height, width = hidden_states.shape
+            hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
+
+        batch_size, sequence_length, _ = (
+            hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
+        )
+        attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
+
+        if attn.group_norm is not None:
+            hidden_states = attn.group_norm(hidden_states.transpose(1, 2)).transpose(1, 2)
+
+        query = attn.to_q(hidden_states)
+
+        if encoder_hidden_states is None:
+            encoder_hidden_states = hidden_states
+        elif attn.norm_cross:
+            encoder_hidden_states = attn.norm_encoder_hidden_states(encoder_hidden_states)
+
+        key = attn.to_k(encoder_hidden_states)
+        value = attn.to_v(encoder_hidden_states)
+
+        query = attn.head_to_batch_dim(query)
+        key = attn.head_to_batch_dim(key)
+        value = attn.head_to_batch_dim(value)
+
+        attention_probs = attn.get_attention_scores(query, key, attention_mask)
+
+        res = int(np.sqrt(attention_probs.shape[1]))
+        num_tokens = attention_probs.shape[-1]
+        # self.attention_maps.append(attention_probs.view(-1, res, res, num_tokens))
+        attention_map = attention_probs.reshape(-1, res, res, num_tokens)
+        if self.sum_attention_maps is None:
+            self.sum_attention_maps = attention_map
+        else:
+            self.sum_attention_maps += attention_map
+        self.counter += 1
+        # self.hidden_states.append(hidden_states)
+        # self.encoder_hidden_states.append(encoder_hidden_states)
+        # self.attention_mask.append(attention_mask)
+
+        hidden_states = torch.bmm(attention_probs, value)
+        hidden_states = attn.batch_to_head_dim(hidden_states)
+
+        # linear proj
+        hidden_states = attn.to_out[0](hidden_states)
+        # dropout
+        hidden_states = attn.to_out[1](hidden_states)
+
+        if input_ndim == 4:
+            hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
+
+        if attn.residual_connection:
+            hidden_states = hidden_states + residual
+
+        hidden_states = hidden_states / attn.rescale_output_factor
+
+        return hidden_states
--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
-# def register_
+import torch
+import requests
+from io import BytesIO
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def load_image(url, size=None):
+    response = requests.get(url, timeout=1)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    if size is not None:
+        img = img.resize(size)
+    return img
+
+# def check_memory():
+#     process = psutil.Process(os.getpid())
+#     print(f"Memory used: {process.memory_info().rss / (1024 * 1024):.2f} MB")
+
+def plot_loss(loss: list):
+    plt.plot(loss_list, label='Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss Value')
+    plt.title('Loss vs Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+def get_tensor_size(tensor: torch.Tensor) -> int:
+    return tensor.element_size() * tensor.nelement() / (1024 * 1024)
